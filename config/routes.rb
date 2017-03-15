c0185845b12917514a80a0f53c8aab8184923a78
linfgit <linfgg1@gmail.com>
2017-03-15 14:46:11 +0800

    create geops index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  #为什么建立groups 的页面时，要加resources 这个指令呢？如果groups
+  #不加这一条groups 页面就会报错。而welcome 则不需要加。
+  root 'groups#index'
+  resources :groups
 end
