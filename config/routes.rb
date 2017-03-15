9b3fbda184ff482b9ab2f597d77d16e25a1f8d23
linfgit <linfgg1@gmail.com>
2017-03-15 15:44:52 +0800

    user can login/logout/signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #为什么建立groups 的页面时，要加resources 这个指令呢？如果groups
   #不加这一条groups 页面就会报错。而welcome 则不需要加。
